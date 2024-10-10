fd557d4acdc7bd49a8c0ba8aeb6aba5adf3c9c6f
afrim <afrim@Xubuntu.myguest.virtualbox.org>
2024-10-10 14:36:47 -0500

    Updated to include exporting to .json

--- A3/server.py
+++ A3/server.py
@@ -3,10 +3,12 @@ from iperf3 import Server
 
 def run_server():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', type=str, required=True, help='Server IP address')
     parser.add_argument('--port', type=int, default=5201, help='iPerf server port')
     args = parser.parse_args()
 
     server = Server()
+    server.bind_address = args.ip
     server.port = args.port
     server.run()
 
